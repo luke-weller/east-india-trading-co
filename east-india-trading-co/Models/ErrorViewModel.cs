42680fb6b7ca98fac721f142879dc46a315ecf96
Luke Weller <33352024+myselfweller@users.noreply.github.com>
2022-05-19 08:14:38 +0100

    Add project files.

---
+++ east-india-trading-co/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace east_india_trading_co.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
